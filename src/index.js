a04ca6e45e310cd04b8aabe5720a96c2e00d1a01
Lunerr <kyleroe94@gmail.com>
2018-04-04 01:07:59 -0500

    guild specific items

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const patron = require('patron.js');
 const client = require('./singletons/client.js');
 const IntervalService = require('./services/IntervalService.js');
 const registry = require('./singletons/registry.js');
-const credentials = require('./credentials.json');
 const db = require('./database');
 
 client.registry = registry;
